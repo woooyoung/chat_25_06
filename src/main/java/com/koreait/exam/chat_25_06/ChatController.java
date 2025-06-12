ecbbefc4fb0593fd70ae665e0ea1d3e59cbefda9
woooyoung <axdsw121@gmail.com>
2025-06-12 14:16:54 +0900

    messagesResponse record 생성

--- src/main/java/com/koreait/exam/chat_25_06/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/ChatController.java
@@ -1,7 +1,5 @@
 package com.koreait.exam.chat_25_06;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,20 +12,13 @@ public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
-    public record writeChatMessageResponse(long id, String authorName, String content) {
-
-    }
-
-//    @AllArgsConstructor
-//    @Getter
-//    public static class writeChatMessageRequest {
-//        private final String authorName;
-//        private final String content;
-//    }
 
     public record writeChatMessageRequest(String authorName, String content) {
     }
 
+    public record writeChatMessageResponse(long id, String authorName, String content) {
+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
@@ -43,14 +34,18 @@ public class ChatController {
         );
     }
 
+    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
+
+    }
+
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<List<ChatMessage>> showMessages() {
+    public RsData<messagesResponse> showMessages() {
 
         return new RsData<>(
                 "S-1",
                 "성공",
-                chatMessages
+                new messagesResponse(chatMessages, chatMessages.size())
         );
     }
 }
