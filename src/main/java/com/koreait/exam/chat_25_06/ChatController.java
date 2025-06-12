d66bc116c69848ebefbc1ffdbd2354ac289f06da
woooyoung <axdsw121@gmail.com>
2025-06-12 13:48:59 +0900

    메세지 작성시 작성자, 내용을 JSON 형태로 전달

--- src/main/java/com/koreait/exam/chat_25_06/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/ChatController.java
@@ -1,5 +1,7 @@
 package com.koreait.exam.chat_25_06;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +14,32 @@ public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
-    public record writeChatMessageResponse(long id) {
+    public record writeChatMessageResponse(long id, String authorName, String content) {
 
+    }
+
+//    @AllArgsConstructor
+//    @Getter
+//    public static class writeChatMessageRequest {
+//        private final String authorName;
+//        private final String content;
+//    }
+
+    public record writeChatMessageRequest(String authorName, String content) {
     }
 
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<writeChatMessageResponse> writeMessage() {
-        ChatMessage message = new ChatMessage("홍길동", "안녕");
+    public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
+        ChatMessage message = new ChatMessage(req.authorName, req.content);
 
         chatMessages.add(message);
 
         return new RsData<>(
                 "S-1",
                 "메세지가 작성됨",
-                new writeChatMessageResponse(message.getId())
+                new writeChatMessageResponse(message.getId(), message.getAuthorName(), message.getContent())
         );
     }
 
